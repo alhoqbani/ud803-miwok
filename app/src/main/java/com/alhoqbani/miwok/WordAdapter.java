c6c9d3ff89a0baa884ea874be55a4dfd9e1eddd0
Hamoud Alhoqbani <h.alhoqbani@gmail.com>
2018-08-09 00:50:48 -0700

    Modify WordAdapter to add Image associated with the word

--- app/src/main/java/com/alhoqbani/miwok/WordAdapter.java
+++ app/src/main/java/com/alhoqbani/miwok/WordAdapter.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -45,10 +46,10 @@ public class WordAdapter extends ArrayAdapter<Word> {
         numberTextView.setText(currentWord.getDefaultTranslation());
 
         // Find the ImageView in the list_item.xml layout with the ID list_item_icon
-//        ImageView iconView = (ImageView) listItemView.findViewById(R.id.list_item_icon);
+        ImageView iconView = listItemView.findViewById(R.id.image);
         // Get the image resource ID from the current Word object and
         // set the image to iconView
-//        iconView.setImageResource(currentWord.getImageResourceId());
+        iconView.setImageResource(currentWord.getImageResourceId());
 
         // Return the whole list item layout (containing 2 TextViews and an ImageView)
         // so that it can be shown in the ListView
